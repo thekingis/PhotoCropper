018fd7f6e31459b46505c59c4bd184f2d5fc5aa4
Kanayo Austin <lilkanzy@gmail.com>
2024-09-25 14:50:30 +0100

    PhotoCropper

---
+++ app/src/main/java/com/kanasoft/photo/cropper/MainActivity.java
@@ -0,0 +1,18 @@
+package com.kanasoft.photo.cropper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
